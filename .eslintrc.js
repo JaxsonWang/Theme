690f1ecacabdb32921dfb46651c07689af91a341
Jaxson Wang <i@iiong.com>
2020-08-10 19:52:20 +0800

    😊 第一次更新

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+  root: true,
+  parserOptions: {
+    parser: 'babel-eslint',
+    sourceType: 'module'
+  },
+  env: {
+    browser: true,
+    node: true,
+    es6: true
+  },
+  extends: ['eslint:recommended'],
+
+  // add your custom rules here
+  rules: {
+  }
+}
