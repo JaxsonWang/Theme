782867c8b6f66ccd5c1257aae83d79cd70982888
JasxonWang <i@iiong.com>
2020-08-12 22:54:44 +0800

    😜 修复一些问题

--- config/webpack.dev.babel.js
+++ config/webpack.dev.babel.js
@@ -97,6 +97,9 @@ module.exports = {
         exclude: /node_modules/,
         use: [
           {
+            loader: 'style-loader'
+          },
+          {
             loader: 'css-loader',
             options: {
               sourceMap: true,
