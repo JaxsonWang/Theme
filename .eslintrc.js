7af8ebb91bf9d333d3903fc7a7f28807ad2b49d5
JasxonWang <i@iiong.com>
2020-08-15 21:58:01 +0800

    ☺️ 08.15 更新布局信息

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,5 +14,7 @@ module.exports = {
 
   // add your custom rules here
   rules: {
+    'no-unused-vars': 'off',
+    'no-undef': 'off',
   }
 }
--- src/js/home/dynamicColorCover.js
+++ src/js/home/dynamicColorCover.js
@@ -7,16 +7,22 @@ import staticFile from '../static'
 import { loadScripts } from '../utils'
 
 const loadColorClover = window => {
-  console.log('我执行了相关函数', window)
+  const document = window.document
+  const colorThief = new ColorThief()
+  document.querySelectorAll('.post-area').forEach(block => {
+    const color = colorThief.getPalette(block.querySelector('img'), 2)
+    // block.style.backgroundImage = `-webkit-gradient(linear, left top, right bottom, from(rgb(${color[0].join(', ')})), to(rgb(${color[1].join(', ')})))`
+    // block.style.backgroundImage = `linear-gradient(to bottom right, rgb(${color[0].join(', ')}), rgb(${color[1].join(', ')}))`
+  })
 }
 
 const dynamicColorCover = window => {
-  loadScripts([{
-    id: 'color-thief',
-    url: staticFile.colorthief
-  }]).then(() => {
-    loadColorClover(window)
-  })
+  // loadScripts([{
+  //   id: 'color-thief',
+  //   url: staticFile.colorthief
+  // }]).then(() => {
+  //   loadColorClover(window)
+  // })
 }
 
 export default dynamicColorCover(window)
